b6ab6ec5ba4f855d7e47f7bcad2181acf138b922
Muhammad Ramadhan <muhrama082@gmail.com>
2019-10-02 03:17:19 +0700

    deploy

---
+++ precache-manifest.4ff75b1dc6ee155c299cd1e5f9a02811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acec8520b16e8988d81a",
+    "url": "/static/css/main.34996ca4.chunk.css"
+  },
+  {
+    "revision": "acec8520b16e8988d81a",
+    "url": "/static/js/main.acec8520.chunk.js"
+  },
+  {
+    "revision": "66084863ce3be4b79880",
+    "url": "/static/js/1.66084863.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e0d3a5e15450eca1cb9733bb66b0a95",
+    "url": "/index.html"
+  }
+];
